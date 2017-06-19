1a33c4e3217e4fccf856f901bdc8c27cec64fee4
Prateek Gogia <gogia.prateek@hotmail.com>
2017-06-20 01:55:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var app = angular.module('musicApp', []);
 
 app.controller('music', function ($scope, $http) {
-    $scope.checkObject = function (obj) {
-        return angular.equals({}, obj);
-    };
+    $scope.tracks = [];
     $http.get('http://104.197.128.152:8000/v1/tracks')
         .then(function (response) {
             $scope.tracks = response.data.results;
         });
+});
+app.controller('editGenres', function ($scope, $http) {
+    $scope.genres = [];
+    $scope.add_genre = "";
+    $http.get('http://104.197.128.152:8000/v1/genres?page=60')
+        .then(function (response) {
+            $scope.genres = response.data.results;
+        });
+    $scope.add = function (event) {
+        if (event.keyCode == 13) {
+            $http.post('http://104.197.128.152:8000/v1/genres', { name: $scope.add_genre })
+                .then(function (data) {
+                    $scope.genres = data;
+                    //console.log($scope.add_genre);
+                    $scope.add_genre = '';
+                });
+            $http.get('http://104.197.128.152:8000/v1/genres?page=60')
+                .then(function (response) {
+                    $scope.genres = response.data.results;
+                });
+        }
+    }
 });
