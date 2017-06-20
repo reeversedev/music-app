609635e9f72a735203f0fb1156a1c327c07c49e2
Prateek Gogia <gogia.prateek@hotmail.com>
2017-06-20 20:04:32 +0530

    Made final changes.

--- app.js
+++ app.js
@@ -31,35 +31,85 @@ app.controller('addGenres', function ($scope, $http) {
 });
 app.controller('addTrack', function ($scope, $http) {
     $scope.tracks = [];
-    $scope.track = {
-        title: '',
-        rating: 0,
-        genres: function (data) {
-            [].push.call(this, data);
-        },
-    };
-    $http.get('http://104.197.128.152:8000/v1/tracks?page=48')
+    $scope.add_track = '';
+    $http.get('http://104.197.128.152:8000/v1/tracks?page=50')
         .then(function (response) {
             $scope.tracks = response.data.results;
         });
     $scope.add = function (event) {
-        if (event.keyCode == 13) {
-            $http.post('http://104.197.128.152:8000/v1/tracks', $scope.track)
+            $http.post('http://104.197.128.152:8000/v1/tracks', {
+                title: $scope.add_title,
+                rating: $scope.add_rating,
+            })
                 .then(function (data) {
-                    $scope.track = {};
+                    $scope.genres = data;
+                    //console.log($scope.add_genre);
+                    $scope.add_title = '';
+                    $scope.add_rating = '';
+                    $scope.add_genre = '';
+                }),
+            $http.get('http://104.197.128.152:8000/v1/tracks?page=50')
+                .then(function (response) {
+                    $scope.genres = response.data.results;
                 });
         }
-    }
 });
 app.controller('editGenres', function ($scope, $http) {
+    $http.get('http://104.197.128.152:8000/v1/genres')
+        .then(function (response) {
+            $scope.genres = response.data.results;
+        })
     $scope.add = function (event) {
         if (event.keyCode == 13) {
-            $http.get('http://104.197.128.152:8000/v1/genres/' +$scope.genre_id)
+            $http.get('http://104.197.128.152:8000/v1/genres/' + $scope.genre_id)
                 .then(function (response) {
                     $scope.genres = response.data;
                 });
         }
     }
+    $scope.submit = function () {
+        if ($scope.formName.$valid) {
+            $http.post('http://104.197.128.152:8000/v1/genres/' + $scope.genre_id, { name: $scope.genres.name })
+                .then(function (data) {
+                    $scope.genres.name = data;
+                    $scope.genres.id = '';
+                    $scope.genres.name = '';
+                });
+        }
+        $http.get('http://104.197.128.152:8000/v1/genres')
+            .then(function (response) {
+                $scope.genres = response.data.results;
+            });
+    }
+});
+app.controller('editTrack', function ($scope, $http) {
+    $scope.track = {
+        title: '',
+        rating: '',
+        genre: ''
+    };
+    $http.get('http://104.197.128.152:8000/v1/tracks')
+        .then(function (response) {
+            $scope.tracks = response.data.results;
+        });
 
-})
+    $scope.search = function () {
+        if ($scope.formTrack.$valid) {
+            $http.get('http://104.197.128.152:8000/v1/tracks?title=' + $scope.track_title)
+                .then(function (response) {
+                    $scope.search_tracks = response.data.results;
+                })
+        }
+        $scope.submit = function () {
+            if ($scope.formName.$valid) {
+                $http.post('http://104.197.128.152:8000/v1/tracks/' + $scope.search_tracks[0].id, { id: $scope.search_tracks[0].id, title: $scope.search_tracks[0].title, rating: $scope.search_tracks[0].rating, })
+                    .then(function (data) {
+                        $scope.search_tracks = data;
+                        console.log('Success!');
+                })
+            }
+        }
+    }
+
+});
 
